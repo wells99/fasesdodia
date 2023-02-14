8b5629a9a8462b72ff258d1e338ba36b3c0cba39
Wellington M <76633592+wells99@users.noreply.github.com>
2023-02-14 10:30:36 -0300

    Update Script
    Nessa atualização: Tirei o código de dentro da função "onload" utilizei a função "setInterval" e adicionei um 0 as horas, minutos e segundos sempre que seus dígitos forem menor que 10, para que seja exibido de maneira que apareça sempre em 2 dígitos Ex: (00:00:00).

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-function carregar (){
+const relogio = setInterval(function time() {
     var msg = window.document.getElementById('msg')
     var imagemvariavel = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+
+    if (hora < 10) hora = "0" + hora 
+    if (min < 10) min = "0" + min
+    if (seg < 10) seg = "0" + seg
+
+
+    msg.innerHTML = `${hora} : ${min} : ${seg} ` 
+   
     
-    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >=0 && hora<12 ) {
         //Bom dia !!
         imagemvariavel.src = "m1.png"
@@ -18,4 +27,5 @@ function carregar (){
         imagemvariavel.src = "n1.png"
         document.body.style.background = "#082A2A"
     }
-}
+
+})
